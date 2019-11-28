2d3f1cb5f6b8ca89e922996904954ef2cc94b967
Ruian-Li <ruian.li98@gmail.com>
2019-11-28 16:20:52 +0800

    Added search bar and team data

--- nba-shotchart/src/App.js
+++ nba-shotchart/src/App.js
@@ -3,7 +3,7 @@ Reference:
 npm nba package
 npm d3-shotchart package
 */
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import nba from "nba";
 import * as d3 from "d3";
@@ -13,11 +13,18 @@ import { hexbin } from "d3-hexbin";
 window.d3_hexbin = { 
   hexbin: hexbin 
 };
-  
 
-var jamesId = nba.findPlayer("LeBron James").playerId;
+class App extends Component {
+
+  handleClick() {
+    console.log("entered");
+  var playerName = d3.select("#search-bar").node().value;
+  console.log(playerName);
+var playerId = nba.findPlayer(playerName).playerId;
+var teamId = nba.findPlayer(playerName).teamId;
+
 nba.stats.shots({
-  PlayerID: jamesId
+  PlayerID: playerId
 })
 .then(d => {
   var data = [];
@@ -37,16 +44,163 @@ courtSelection.call(basketballCourt);
 courtSelection.datum(data).call(shotsTaken);
 });
 
+var teamData = [];
+nba.stats.commonTeamRoster({
+  TeamID: teamId
+})
+.then( d => {
+  
+  var roster = d.commonTeamRoster;
+  console.log(roster);
+  var players = [];
+  for (var i = 0; i < roster.length; i++) {
+    players.push(roster[i].player);
+  }
+  console.log(players);
+  for (var k = 0; k < players.length; k++) {
+    if (nba.findPlayer(players[k]) === undefined) {
+      continue;
+    }
+    var id = nba.findPlayer(players[k]).playerId;
+    nba.stats.shots({
+      PlayerID: id
+    })
+    .then(d2 => {
+      var temp = d2.shot_Chart_Detail;
+      for (var j = 0; j < temp.length; j++) {
+        var datum = {
+          x: temp[j].locX / 10 + 25,
+          y: temp[j].locY / 10 + 5,
+          shot_made_flag: temp[j].shotMadeFlag
+        }
+        teamData.push(datum);
+    }
+  })
+  .then(
+    d2 => {
+      
+        var teamCourtSelection = d3.select("#team-shot-chart");
+      var teamBasketballCourt = court().width(1000);
+      var teamShotsTaken = shots().shotRenderThreshold(1).displayToolTips(true).displayType("hexbin");
+      teamCourtSelection.call(teamBasketballCourt);
+      teamCourtSelection.datum(teamData).call(teamShotsTaken);
+      
+    }
+  );
+  }
+});
+  };
+
+  render() {
+    return (
+      <div>
+      <textarea placeholder="Search NBA Player" id="search-bar" align="center"></textarea>
+      <button onClick={this.handleClick}>Submit</button>
+      <h3 align="center">Player Data</h3>
+      <div id="shot-chart">
+      </div>
+      <h3 align="center">Team Data</h3>
+      <div id="team-shot-chart">
+      </div>
+    </div>
+    );
+    
+  }
+}
+
+/*
+d3.select("button").on("click", function() {
+  console.log("entered");
+  var playerName = d3.select("#search-bar").node().value;
+  console.log(playerName);
+var playerId = nba.findPlayer(playerName).playerId;
+var teamId = nba.findPlayer(playerName).teamId;
+
+nba.stats.shots({
+  PlayerID: playerId
+})
+.then(d => {
+  var data = [];
+  var temp = d.shot_Chart_Detail;
+  for (var i = 0; i < temp.length; i++) {
+    var datum = {
+      x: temp[i].locX / 10 + 25,
+      y: temp[i].locY / 10 + 5,
+      shot_made_flag: temp[i].shotMadeFlag
+    }
+    data.push(datum);
+  }
+var courtSelection = d3.select("#shot-chart");
+var basketballCourt = court().width(1000);
+var shotsTaken = shots().shotRenderThreshold(1).displayToolTips(true).displayType("hexbin");
+courtSelection.call(basketballCourt);
+courtSelection.datum(data).call(shotsTaken);
+});
+
+var teamData = [];
+nba.stats.commonTeamRoster({
+  TeamID: teamId
+})
+.then( d => {
+  
+  var roster = d.commonTeamRoster;
+  console.log(roster);
+  var players = [];
+  for (var i = 0; i < roster.length; i++) {
+    players.push(roster[i].player);
+  }
+  console.log(players);
+  for (var k = 0; k < players.length; k++) {
+    if (nba.findPlayer(players[k]) === undefined) {
+      continue;
+    }
+    var id = nba.findPlayer(players[k]).playerId;
+    nba.stats.shots({
+      PlayerID: id
+    })
+    .then(d2 => {
+      var temp = d2.shot_Chart_Detail;
+      for (var j = 0; j < temp.length; j++) {
+        var datum = {
+          x: temp[j].locX / 10 + 25,
+          y: temp[j].locY / 10 + 5,
+          shot_made_flag: temp[j].shotMadeFlag
+        }
+        teamData.push(datum);
+    }
+  })
+  .then(
+    d2 => {
+      
+        var teamCourtSelection = d3.select("#team-shot-chart");
+      var teamBasketballCourt = court().width(1000);
+      var teamShotsTaken = shots().shotRenderThreshold(1).displayToolTips(true).displayType("hexbin");
+      teamCourtSelection.call(teamBasketballCourt);
+      teamCourtSelection.datum(teamData).call(teamShotsTaken);
+      
+    }
+  );
+  }
+});
+});
   
 function App() {
 
   return (
-    
+      
+    <div>
+      
+      <textarea placeholder="Search NBA Player" id="search-bar" align="center"></textarea>
+      <button>Submit</button>
+      <h3 align="center">Player Data</h3>
       <div id="shot-chart">
       </div>
-  
+      <h3 align="center">Team Data</h3>
+      <div id="team-shot-chart">
+      </div>
+    </div>
   );
   
 }
-
+*/
 export default App;
