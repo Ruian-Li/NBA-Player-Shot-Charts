1bbadf6dac2b81dcd53903d008316c9f8a1233f1
Ruian-Li <ruian.li98@gmail.com>
2019-12-09 22:20:32 -0600

    Final submission

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ npm start
 
 The data is within the nba npm package (which should be installed with the first
 command above)
+
+To resolve the CORS issue, run this on Safari. Settings: go to Safari - Preferences - Advanced, check "Show Develop menu in menu bar", go to Develop and click "Disable Cross-Origin Restrictions".
--- nba-shotchart/src/App.js
+++ nba-shotchart/src/App.js
@@ -16,17 +16,17 @@ window.d3_hexbin = {
 
 class App extends Component {
 
-  handleClick() {
-    console.log("entered");
+handleClick() {
   var playerName = d3.select("#search-bar").node().value;
-  console.log(playerName);
-var playerId = nba.findPlayer(playerName).playerId;
-var teamId = nba.findPlayer(playerName).teamId;
+  var playerId = nba.findPlayer(playerName).playerId;
+  var teamId = nba.findPlayer(playerName).teamId;
 
 nba.stats.shots({
-  PlayerID: playerId
+  PlayerID: playerId,
+  Season: "2019-20"
 })
 .then(d => {
+  console.log(d);
   var data = [];
   var temp = d.shot_Chart_Detail;
   for (var i = 0; i < temp.length; i++) {
@@ -46,6 +46,7 @@ courtSelection.datum(data).call(shotsTaken);
 
 var teamData = [];
 nba.stats.commonTeamRoster({
+  Season: "2019-20",
   TeamID: teamId
 })
 .then( d => {
@@ -63,7 +64,8 @@ nba.stats.commonTeamRoster({
     }
     var id = nba.findPlayer(players[k]).playerId;
     nba.stats.shots({
-      PlayerID: id
+      PlayerID: id,
+      Season: "2019-20"
     })
     .then(d2 => {
       var temp = d2.shot_Chart_Detail;
@@ -78,13 +80,11 @@ nba.stats.commonTeamRoster({
   })
   .then(
     d2 => {
-      
-        var teamCourtSelection = d3.select("#team-shot-chart");
+      var teamCourtSelection = d3.select("#team-shot-chart");
       var teamBasketballCourt = court().width(1000);
       var teamShotsTaken = shots().shotRenderThreshold(1).displayToolTips(true).displayType("hexbin");
       teamCourtSelection.call(teamBasketballCourt);
       teamCourtSelection.datum(teamData).call(teamShotsTaken);
-      
     }
   );
   }
@@ -104,103 +104,7 @@ nba.stats.commonTeamRoster({
       </div>
     </div>
     );
-    
   }
 }
 
-/*
-d3.select("button").on("click", function() {
-  console.log("entered");
-  var playerName = d3.select("#search-bar").node().value;
-  console.log(playerName);
-var playerId = nba.findPlayer(playerName).playerId;
-var teamId = nba.findPlayer(playerName).teamId;
-
-nba.stats.shots({
-  PlayerID: playerId
-})
-.then(d => {
-  var data = [];
-  var temp = d.shot_Chart_Detail;
-  for (var i = 0; i < temp.length; i++) {
-    var datum = {
-      x: temp[i].locX / 10 + 25,
-      y: temp[i].locY / 10 + 5,
-      shot_made_flag: temp[i].shotMadeFlag
-    }
-    data.push(datum);
-  }
-var courtSelection = d3.select("#shot-chart");
-var basketballCourt = court().width(1000);
-var shotsTaken = shots().shotRenderThreshold(1).displayToolTips(true).displayType("hexbin");
-courtSelection.call(basketballCourt);
-courtSelection.datum(data).call(shotsTaken);
-});
-
-var teamData = [];
-nba.stats.commonTeamRoster({
-  TeamID: teamId
-})
-.then( d => {
-  
-  var roster = d.commonTeamRoster;
-  console.log(roster);
-  var players = [];
-  for (var i = 0; i < roster.length; i++) {
-    players.push(roster[i].player);
-  }
-  console.log(players);
-  for (var k = 0; k < players.length; k++) {
-    if (nba.findPlayer(players[k]) === undefined) {
-      continue;
-    }
-    var id = nba.findPlayer(players[k]).playerId;
-    nba.stats.shots({
-      PlayerID: id
-    })
-    .then(d2 => {
-      var temp = d2.shot_Chart_Detail;
-      for (var j = 0; j < temp.length; j++) {
-        var datum = {
-          x: temp[j].locX / 10 + 25,
-          y: temp[j].locY / 10 + 5,
-          shot_made_flag: temp[j].shotMadeFlag
-        }
-        teamData.push(datum);
-    }
-  })
-  .then(
-    d2 => {
-      
-        var teamCourtSelection = d3.select("#team-shot-chart");
-      var teamBasketballCourt = court().width(1000);
-      var teamShotsTaken = shots().shotRenderThreshold(1).displayToolTips(true).displayType("hexbin");
-      teamCourtSelection.call(teamBasketballCourt);
-      teamCourtSelection.datum(teamData).call(teamShotsTaken);
-      
-    }
-  );
-  }
-});
-});
-  
-function App() {
-
-  return (
-      
-    <div>
-      
-      <textarea placeholder="Search NBA Player" id="search-bar" align="center"></textarea>
-      <button>Submit</button>
-      <h3 align="center">Player Data</h3>
-      <div id="shot-chart">
-      </div>
-      <h3 align="center">Team Data</h3>
-      <div id="team-shot-chart">
-      </div>
-    </div>
-  );
-  
-}
-*/
 export default App;
